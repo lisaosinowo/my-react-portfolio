b86f9393035add9ac20d8c88bbbd92a14772f9a5
lisaosinowo <liseozzy@hotmail.co.uk>
2025-06-02 13:01:30 +0100

    Used useState to change the display for each section

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import Feed from '../sections/Feed';
-import About from '../sections/About';
-// import Projects from '../sections/Projects';
+import { useState } from "react";
+import Feed from "../sections/Feed";
+import About from "../sections/About";
+import Projects from "../sections/Projects";
+import SectionButton from "./SectionButton";
 
 const MainSection = () => {
+   const [active, setActive] = useState("feed");
+
+   const showSection = () => {
+      switch (active) {
+         case "feed":
+            return <Feed />;
+         case "about":
+            return <About />;
+         case "projects":
+            return <Projects />;
+         default:
+            return <Feed />;
+      }
+   };
+
    return (
       <>
-         <Feed />
-         <About />
-         {/* <Projects /> */}
+         <SectionButton onSelect={setActive} />
+         <div>{showSection()}</div>
       </>
    );
-}
+};
 
 export default MainSection;
