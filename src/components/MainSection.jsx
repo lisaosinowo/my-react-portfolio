9932f96918e6bd17224a4452d40c43bbe3c564ae
lisaosinowo <liseozzy@hotmail.co.uk>
2025-06-02 12:06:18 +0100

    Added the about and projects section

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Feed from '../sections/Feed';
+import About from '../sections/About';
+// import Projects from '../sections/Projects';
 
 const MainSection = () => {
    return (
       <>
          <Feed />
+         <About />
+         {/* <Projects /> */}
       </>
    );
 }
