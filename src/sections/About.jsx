dc31989c0f1f6cf2de57ae22289e63a16d2b4dd2
lisaosinowo <liseozzy@hotmail.co.uk>
2025-06-02 12:05:31 +0100

    Added the about section

---
+++ src/sections/About.jsx
@@ -0,0 +1,46 @@
+import "../styles/_about.scss";
+
+const About = () => {
+   return (
+      <>
+         <section id="aboutMaterial" class="about-section">
+            <div className="about-main-container">
+               <div className="about-container">
+                  <h1>Hi User! 😊</h1>
+                  <p className="about-info">
+                     I am Lisa, a Software Enginner (and Chemical Engineering
+                     graduate).
+                  </p>
+
+                  <p className="about-info">
+                     My Chemical Engineering degree is where my problem solving
+                     and innovation skills stemmed from. After obtaining my
+                     degree I decided to transition into the tech world where I
+                     initially taught myself how to code with Python. In October
+                     2024, I obtained a Frontend Development Fundamentals
+                     certificate after completing my Frontend Development
+                     bootcamp. Within the bootcamp, I learned HTML and CSS. Now
+                     I am learning JavaScript and React to further equip myself
+                     with the skills needed for Frontend Development.
+                  </p>
+
+                  <p className="about-info">
+                     In January 2025 I started a 16-week CFGdegree in
+                     Fullstack Development with a +Masters in Cyber Security
+                     with Code First Girls. This course included HTML, CSS,
+                     JavaScript, React, TypeScript and Angular.
+                  </p>
+
+                  <p className="about-info">
+                     I am known to be a resilient and determinted character who
+                     doesn't shy away from complex problems/tasks. I am always
+                     up for a challenge which also fuels my coding confidence.
+                  </p>
+               </div>
+            </div>
+         </section>
+      </>
+   );
+};
+
+export default About;
