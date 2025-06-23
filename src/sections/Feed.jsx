7f53dcfd324af43cd8624c4e6ca7e97bb79e8dd2
lisaosinowo <liseozzy@hotmail.co.uk>
2025-06-23 16:51:49 +0100

    Removed and added a new feed input

--- src/sections/Feed.jsx
+++ src/sections/Feed.jsx
@@ -6,9 +6,9 @@ const Feed = () => {
    return (
       <>
          <FeedItemPinned />
-         <FeedItem date='09 June 2025' feedText='Starting the Cybersecurity Final Project today with my group. We have to write a report on the security vulnerabilities of our CFGDegree Group Project. #ThinkLikeASecurityAnalyst' />
+         <FeedItem date='19 June 2025' feedText='Cybersecurity final project finished ✅ CFG +Masters finished ✅' />
+         <FeedItem date='09 June 2025' feedText='Starting the cybersecurity final project today with my group. We have to write a report on the security vulnerabilities of our CFGDegree Group Project. #ThinkLikeASecurityAnalyst' />
          <FeedItem date='29 May 2025' feedText='I got a MERIT in my Fullstack Development CFG Degree 🥳 I have come a long way from Chemical Engineering 💃🏾' />
-         <FeedItem date='21 May 2025' feedText='Learning about Kali Linux and currently have it working. It seems easy to use so far.. Although I still cannot believe that this can be used for penetration testing 😯 💻' />
          <FeedItem date='19 May 2025' feedText='Today is the day that I start the CFG +Masters in Cybersecurity 👩🏾‍💻. However, I have been told that it is a step up from the CFG Degree. 🥲 #wishmeluck' />
          <FeedItem date='9 May 2025' feedText='After 16 weeks of intense learning I have finished the CFG Degree in Fullstack Development! #BigAchievement 🥳' />
          <FeedItem date='4 May 2025' feedText='Allow me to introduce to you all... MOODIE 🎦 An app that gives you movie recommendations based on your mood! You can either take a Mood Quiz or use the Random Movie Generator.' image={Moodie} />
