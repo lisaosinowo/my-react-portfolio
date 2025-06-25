39729d6aae14b0ff61be658df67350fcaccee25e
lisaosinowo <liseozzy@hotmail.co.uk>
2025-06-25 19:55:35 +0100

    Wrapped body-container in another container

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,20 @@
-import ProfileInfo from './ProfileInfo';
-import '../styles/_mixins.scss';
-import '../styles/_body.scss';
-import MainSection from './MainSection';
+import ProfileInfo from "./ProfileInfo";
+import "../styles/_mixins.scss";
+import "../styles/_body.scss";
+import MainSection from "./MainSection";
 
 const Body = () => {
    return (
       <>
-         <div className="body-container">
-            <ProfileInfo />
-            <MainSection />
+         <div className="body-main-container">
+            <div className="body-container">
+               <ProfileInfo />
+               <MainSection />
+            </div>
          </div>
+         
       </>
    );
-}
+};
 
 export default Body;
